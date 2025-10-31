83040242d2ac3b9edf90c3ddb348165473fb2fe1
Miguel Schionato <migueloliveira642@hotmail.com>
2025-10-31 02:20:12 +0100

    Fixed waybar bugs

--- waybar/launch_waybar.sh
+++ waybar/launch_waybar.sh
@@ -8,21 +8,25 @@ RESOLUTION=$(echo "$MONITOR_INFO" | grep -oP '\d+x\d+' | head -n 1)
 WIDTH=$(echo "$RESOLUTION" | cut -d'x' -f1)
 
 CONFIG_DIR="$HOME/.config/waybar"
+STYLE_FILE="$CONFIG_DIR/style.css"
 CONFIG_FILE="$CONFIG_DIR/config.jsonc"
 
 # Remove existing symlink if any
+rm -f "$STYLE_FILE"
 rm -f "$CONFIG_FILE"
 
 if [ "$WIDTH" -eq 1920 ]; then
-    ln -s "$CONFIG_DIR/config-1920.jsonc" "$CONFIG_FILE"
+    ln -s "$CONFIG_DIR/style-1080.css" "$STYLE_FILE"
+    ln -s "$CONFIG_DIR/config-1080.jsonc" "$CONFIG_FILE"
     echo "Using config-1920.jsonc"
 elif [ "$WIDTH" -eq 2880 ]; then
-    ln -s "$CONFIG_DIR/config-2880.jsonc" "$CONFIG_FILE"
+    ln -s "$CONFIG_DIR/style-2880.css" "$STYLE_FILE"
+    ln -s "$CONFIG_DIR/config-2880.jsonc" "$STYLE_FILE"
     echo "Using config-2880.jsonc"
 else
     # Fallback for unknown resolutions
     echo "Unknown resolution: $RESOLUTION. Using default config-1920.jsonc as fallback."
-    ln -s "$CONFIG_DIR/config-1920.jsonc" "$CONFIG_FILE"
+    ln -s "$CONFIG_DIR/style-1080.css" "$STYLE_FILE"
 fi
 
 # Launch Waybar
